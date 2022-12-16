a88b8fd5532ba51332d40068a5d8d0530d2fc598
Mike Brewer <michaelandrewbrewer@gmail.com>
2022-12-16 21:50:28 +0000

    finished getting older, getting unhealthier feature

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -31,8 +31,39 @@ describe('constructor', () => {
           pet.growUp();
       
           expect(pet.age).toEqual(1);
+        
+       
+        })
+        it('expect hunger to be 0', () => {
+            const pet = new Pet('Greg');
+            
+            expect(pet.hunger).toEqual(0);
         });
-      });
 
+        it('growUp increases hunger by 5', () => {
+            const pet = new Pet('Chris');
+
+            pet.growUp();
+
+            expect(pet.hunger).toEqual(5);
+        })
+       
+        it('expect fitness to be 10', () => {
+            const pet = new Pet('Fitty');
+
+            expect(pet.fitness).toEqual(10);
+
+        })
+
+        it('growUp decreases fitness by 3', () => {
+            const pet = new Pet('Wendy');
+
+            pet.growUp();
+
+            expect(pet.fitness).toEqual(7);
+        })
+      });
+     
+    
   });
   
